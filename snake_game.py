3937840d117be2108b29918fb7f072bf05223d08
ankitjha22003 <ankitjha27140@gmail.com>
2025-08-11 13:10:55 +0530

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -14,7 +14,7 @@ dis_width = 600
 dis_height = 400
 
 dis = pygame.display.set_mode((dis_width,dis_height))
-pygame.display.set_caption("Snake game")
+pygame.display.set_caption("Snake game") #caption ke liye dene ke liye krte hai
 
 clock = pygame.time.Clock()
 
